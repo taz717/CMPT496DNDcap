5e6d8548cf974f3c01041d4bf160892f37f70b9b
Moataz Khallaf <MoatazKhallaf@outlook.com>
2023-10-05 21:11:45 -0600

    finish backend methods
    - add schema
    - validate incoming objects to server
    - add character:
    * delete
    * add
    * update
    * find 1
    * find all

--- imports/ui/components/CharacterCreator.jsx
+++ imports/ui/components/CharacterCreator.jsx
@@ -1,3 +1,11 @@
 import React from "react";
 import { CharacterCollection } from "../api/CharacterCollection";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
+
+export const CharacterCreator = () => {
+	// State
+	const handleChangeInput = (e) => {
+		const { name, value } = e.target;
+		setState((prevState) => ({ ...prevStatestate, [name]: value }));
+	};
+};
