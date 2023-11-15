303de2abb312d03b4c9942d89a83439fea9df09f
Asernecky <asernecky@gmail.com>
2023-11-15 13:46:43 -0700

    Sign In and Sign up screens
    No functionality, just display of screens

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,6 +11,8 @@ import { Nav } from "./components/Nav.jsx";
 // make sure to follow the same format as the other pages
 import { Hello } from "./pages/Hello.jsx";
 import { Info } from "./pages/Info.jsx";
+import SignIn from "./pages/sign-in/SignIn.js";
+import SignUp from "./pages/account-create/SignUp.js";
 
 // App/Router for this project, it will be the same thing
 export const App = () => {
@@ -18,7 +20,8 @@ export const App = () => {
 		<div>
 			<Nav />
 			<Routes>
-				<Route path="/" element={<Hello />} />
+				<Route path="/" element={<SignIn />} />
+				<Route path="/signup" element={<SignUp />} />
 				<Route path="/info" element={<Info />} />
 			</Routes>
 		</div>
