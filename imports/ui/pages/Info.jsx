3786102aa42d48e96b2e0abd3a41e0a4694c7c3f
DangDoggit <musilek.dave@gmail.com>
2023-12-04 22:04:00 -0700

    Pullin

---
+++ imports/ui/pages/Info.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import SpellDisplay from '/components/SpellDisplay.jsx'
+import { useFind, useSubscribe } from "meteor/react-meteor-data";
+
+export const Info = () => {
+	return SpellDisplay;
+};
